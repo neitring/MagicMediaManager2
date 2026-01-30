7ba641e70ff21e3b6c261b3712f84ca438a38326
Jay Neitring <jay.neitring@gmail.com>
2026-01-30 12:07:59 -0500

    Initial Commit

---
+++ MagicMediaManager/MagicMediaManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicMediaManagerApp.swift
+//  MagicMediaManager
+//
+//  Created by Jay Neitring on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct MagicMediaManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
